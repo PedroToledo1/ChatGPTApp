cdca8076116dcc73d476d9b9f1704d15cf184c35
PedroToledo1 <59672144+PedroToledo1@users.noreply.github.com>
2023-08-07 19:28:39 -0300

    Initial Commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Pedro Toledo on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
